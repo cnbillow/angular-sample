c2a00d217ace4e55d5ca9dd1099ef31096297b4e
Gustavo Zavaleta Vargas <mzavaletavargas@gmail.com>
2018-04-26 00:22:38 -0500

    details

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -9,14 +9,14 @@ export function loadModule(module) {
 }
 
 const routes: Routes = [
-  {
+/*   {
     path: '',
     // temporary until The bug is fixed on Angular
     loadChildren: './home/home.module#HomeModule',
-    /* loadChildren: loadModule(HomeModule), */
-  },
+    // loadChildren: loadModule(HomeModule), 
+  }, */
   {
-    path: 'users',
+    path: '',
     loadChildren: './users/users.module#UsersModule',
     /* loadChildren: loadModule(UsersModule), */
   },
