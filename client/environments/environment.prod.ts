5f103cfb4f74d03c8783dcaf2f09e6222eeb4e36
Gustavo Zavaleta Vargas <mzavaletavargas@gmail.comv>
2018-05-09 19:21:23 -0500

    adding firebase config

--- client/environments/environment.prod.ts
+++ client/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDn6yUq_WnnR4EMYrjV_T-wpMKdpydFXXk',
+    authDomain: 'angular-sample-c7b90.firebaseapp.com',
+    projectId: 'project-787416594018',
+  }
 };
